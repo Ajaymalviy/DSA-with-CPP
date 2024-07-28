da557fae800c360870ba22bccd7b896315470624
ajeymalviy <ajeymlaviya143@gmail.com>
2024-07-28 22:48:40 +0530

    added new directory oops which contain all four legs of oops

---
+++ OOPS/Encapsulation.cpp
@@ -0,0 +1,57 @@
+// #include <iostream>
+// using namespace std;
+//encapsulation is the wrapping up of data and member function which is combining of property and member functions.
+// it is also used for data hiding how?? make it private password and balance of class account .
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher{
+	//properties/attributes
+private:
+	float salary;
+
+public:
+	string name;
+	string department;
+	string subject;
+	
+
+	//methods or member functions
+	void changedepartment(string newdepartment){
+		department = newdepartment;
+	}
+    
+    //called setter in cpp bcoz it takes private and print it
+	void setsalary(float s){
+		salary = s;
+		cout<<"this is from setsalary function salary"<<salary<<endl;
+		cout<<"this is from setsalary function s"<<s<<endl;
+	}
+
+    //it called a getter which print salart by jugad
+	// void getsalary(){
+	// 	cout<<"this is from getsalary function i was print salary "<<salary<<endl;
+	// } 
+
+	float getsalary(){
+		return salary;
+	}
+
+};
+
+
+int main(){
+    Teacher t1;
+    t1.name = "ajay";
+    t1.subject = "Maths";
+    t1.setsalary(750000);
+    cout<<t1.name<<endl;
+    cout<<t1.subject<<endl;
+    t1.getsalary();
+    // cout<<t1.salary<<endl; it gives error cozz this is the private member.
+    //  it gives error at first because it is private at first by default.now it comes in picture of access modifier.
+    //which is private ,public, protected.
+	return 0;
+}
