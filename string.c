9fb0131a94ac182bbe7b01f9ff009e43c1894be4
Ajay Malviya <128165665+Ajaymalviy@users.noreply.github.com>
2024-08-03 23:24:49 +0530

    Add files via upload
    added new file of string in c.

---
+++ string.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+struct students
+{
+int rollno;
+char name[20];
+float per;
+};
+struct students S1;
+students s[5]
+int main()
+{
+for(int i=0;i<5;i++)
+}
+printf("\nEnter the rollno:");
+scanf("%d",&S1.rollno);
+printf("Enter name");
+scanf("%s",S1.name);
+printf("Enter persentage");
+scanf("%f",&S1.per);
+for(int j=0;j<5;j++)
+printf("\n roll no is:%d",S1.rollno);
+printf("\n name is:%s",S1.name);
+printf("\n persentage is:%f",S1.per);
+}
