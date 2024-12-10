c77bb9c26f9aa51453a5193b251500dad4e95122
t <ajeymalviya143@gmail.com>
2024-12-10 13:28:00 +0530

    added or deleted without extension files

---
+++ STL.cpp/tryy.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double p=3234.143454353454352d;
+    cout<<p<<endl;
+    cout<<sizeof(p)<<endl;
+
+    char value ='A';
+    int j=value;
+    cout<<j<<endl;
+
+    return 0;
+}
