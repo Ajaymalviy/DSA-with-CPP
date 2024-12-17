a9f17b09d9f5c77d318508a15bace7bf8a400fae
t <ajeymalviya143@gmail.com>
2024-12-17 23:58:27 +0530

    added new file priority heap as adding functional

---
+++ priority_heap_mix.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<functional>
+using namespace std;
+
+
+int main(){
+    plus<int> aj;
+    int sum = aj(3,4);
+    cout<<"ye dikh rhaa "<<sum<<endl;
+   
+
+
+    minus<int>minus;
+    int res=minus(2,2);
+    cout<<"result of minus is "<<res<<endl;
+
+    divides<int>devide;
+    int ress=devide(5,4);
+    cout<<"divide result is "<<ress<<endl;
+    
+    multiplies<float>mult;
+    float resutl=mult(1.8,2);
+    cout<<"result of mutliple is "<<resutl<<endl;
+
+
+    return 0 ;
+
+
+}
