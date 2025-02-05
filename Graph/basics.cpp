cfc317be05343f19c51e18d7d814b9ab45b847bd
t <ajeymalviya143@gmail.com>
2025-02-05 21:01:54 +0530

    updated file basic in graph

---
+++ Graph/basics.cpp
@@ -0,0 +1,8 @@
+// graph  bas nodes or edges ka connection h nodes wo vertex bhi kehte h .
+#include<iostream>
+using namespace std;
+class Graph{
+    private:
+        vector<vector<int>>edges;
+        
+}
