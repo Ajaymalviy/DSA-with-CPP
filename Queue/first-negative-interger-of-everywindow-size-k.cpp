2df5295c4ee727782bbdb834ae6ea9dc2b017055
t <ajeymalviya143@gmail.com>
2025-07-21 23:04:52 +0530

    added new question of queue where finding first negative no.

---
+++ Queue/first-negative-interger-of-everywindow-size-k.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+#include<queue>
+#include<deque>
+#include<vector>
+
+vector<long long>printFirstNegativeInteger( long long int A[], long long int N, long long int K){
+    
+    deque<long long int>dq;
+    vector<long long>ans;
+
+    //for first window of size k
+    for (int i=0; i<K; i++){
+        if(A[i]<0){
+            dq.push_back(i);
+        }
+        //store answer as well
+        if(dq.size()>0){
+            ans.push_back(A[dq.front()]);
+        }
+        else{
+            ans.push_back(0);
+        }
+    }
+    
+    //for remaining window same logic
+
+    for(int i=K; i<N; i++){
+        //remval logic from front
+        if(!dq.empty() && i-dq.front()>=K){
+            dq.pop_front();
+        }
+        //addition from end 
+        if(A[i]<0){
+            dq.push_back(i);
+        }
+        //answer store for this windows
+        if(dq.size()>0){
+            ans.push_back(A[dq.front()]);
+        }
+        else{
+            ans.push_back(0);
+        }
+
+    }
+
+    return ans;
+
+
+}
+
+int main() {
+    long long A[] = {12, -1, -7, 8, 15, 30, 16, 28};
+    long long N = 8;
+    long long K = 3;
+
+    vector<long long> result = printFirstNegativeInteger(A, N, K);
+    for (auto x : result)
+        cout << x << " ";
+    return 0;
+}
